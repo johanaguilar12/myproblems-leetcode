5c5c076d3fc1aae642cc1df0a05b1aa5395a6ba6
Johan Aguilar <71256753+johanaguilar12@users.noreply.github.com>
2025-09-21 20:11:11 -0600

    Time: 0 ms (100.00%) | Memory: 9.4 MB (17.67%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     string removeDigit(string number, char digit) {
-        vector<string> candidateAnswers;
+        string answer = "000";
         for(int i = 0; i < number.size(); i++){
             if(number[i] == digit){
                 string current = number.substr(0, i);
                 current += number.substr(i+1, number.size());
-                cout << current << endl;
-                candidateAnswers.push_back(current);
+                answer = max(answer, current);
             }
         }
-        return *max_element(candidateAnswers.begin(), candidateAnswers.end());
+        return answer;
     }
 };
